aab3a87de3216b5a34a07a4da40b145404e6fdc1
Bonsi <amonselltapia@gmail.com>
2022-01-13 00:39:58 +0100

    update

--- Quiz/main.js
+++ Quiz/main.js
@@ -1,30 +1,41 @@
-const getQuestions = async function (){
-    try {
-      let res =  await axios.get("https://opentdb.com/api.php?amount=5") 
-    return res.data;
-    } catch (error) {
-        console.log(error)
-    }
-}
-const getTitle = async function(data){
-    console.log(data.results)
-    let arrTitle = data.results;
-    for(let i=0; i<=arrTitle.length; i++){
-        let question = arrTitle[i].question;   
-        document.getElementById(`response${i+1}`).innerHTML = `<h2>${question}<h2>`; 
-}  
-    
-    
-}
-const getResponses = async function(data){
+const getQuestions = async function () {
+  try {
+    let res = await axios.get("https://opentdb.com/api.php?amount=1");
+    return res.data.results[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
+const getTitle = async function (data) {
+  let title = data.question;
+  console.log(data, title);
+  document.getElementById("question").innerHTML = title;
+};
+const getAnswers = async function (data) {
+  let correctAns = data.correct_answer;
+  let falseAns = data.incorrect_answers;
+  let answer = [];
+  answer.push(correctAns);
+  answer = answer.concat(falseAns);
+  console.log(answer);
+  for (let i = 0; i < answer.length; i++) {
+    document.getElementById(
+      "question"
+    ).innerHTML += `<label name="question">${answer[i]}</label> <input id="answer" type="radio" value="${answer[i]}" name="question">`;
+  }
+  let ans = document.getElementById("answer").value;
+  ans.onclick(validateAnswer);
+};
 
+function validateAnswer(rigth, value) {
+  console.log("funciono");
+  if (rigth == value) console.log("cierto");
 }
 
+const quiz = async function () {
+  let data = await getQuestions().then();
+  getTitle(data);
+  getAnswers(data);
+};
 
-const quiz = async function(){
-    let data = await getQuestions().then();
-    let title = await getTitle(data).then()
-    let response =await getResponses(data).then() 
-}
-
-quiz().then()
+quiz().then();
