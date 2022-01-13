ee67f17392209cb35b83fc252b11f8cb975f577f
Bonsi <amonselltapia@gmail.com>
2022-01-14 00:48:59 +0100

    update noche

--- Quiz/main.js
+++ Quiz/main.js
@@ -8,34 +8,48 @@ const getQuestions = async function () {
 };
 const getTitle = async function (data) {
   let title = data.question;
-  console.log(data, title);
-  document.getElementById("question").innerHTML = title;
+  document.getElementById("question").innerHTML = `<h1>${title}</h1>`;
 };
 const getAnswers = async function (data) {
   let correctAns = data.correct_answer;
+  let encondedCorrectAns = window.btoa(correctAns);
   let falseAns = data.incorrect_answers;
   let answer = [];
   answer.push(correctAns);
-  answer = answer.concat(falseAns);
-  console.log(answer);
+  answer = answer.concat(falseAns).sort();
+  document.getElementById("response").innerHTML = "";
   for (let i = 0; i < answer.length; i++) {
     document.getElementById(
-      "question"
-    ).innerHTML += `<label name="question">${answer[i]}</label> <input id="answer" type="radio" value="${answer[i]}" name="question">`;
+      "response"
+    ).innerHTML += `<div id="answer${i}" onclick=" myvalue('${answer[i]}' ,'${encondedCorrectAns}')" >${answer[i]}</div>`;
   }
-  let ans = document.getElementById("answer").value;
-  ans.onclick(validateAnswer);
 };
-
-function validateAnswer(rigth, value) {
-  console.log("funciono");
-  if (rigth == value) console.log("cierto");
-}
 
 const quiz = async function () {
   let data = await getQuestions().then();
   getTitle(data);
   getAnswers(data);
 };
+function myvalue(value, correctAns) {
+  let bcodeCorrectAns = window.atob(correctAns);
+  let tries = document.getElementById("tries");
+  let right = document.getElementById("right");
+  let currentValueTries = Number(tries.textContent);
+  let currentValueRight = Number(right.textContent);
+  if (value === bcodeCorrectAns) {
+    currentValueRight += 1;
+    right.innerHTML = currentValueRight;
+  }
+  currentValueTries += 1;
+  let totalRight = (currentValueRight * 100) / currentValueTries;
+  let totalWrong =
+    ((currentValueTries - currentValueRight) * 100) / currentValueTries;
+  document.getElementById("right").style.width = `${totalRight}%`;
+  document.getElementById("right").style.borderRadius = `10px`;
+
+  document.getElementById("tries").style.width = `${totalWrong}%`;
+  tries.innerHTML = currentValueTries;
+  quiz().then();
+}
 
 quiz().then();
